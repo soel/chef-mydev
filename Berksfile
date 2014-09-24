bdd3628cfa5b02a9f6e3ee7fb11dbc5943e43fb0
kanbara <soel754@gmail.com>
2014-09-24 11:22:42 +0900

    add test

--- Berksfile
+++ Berksfile
@@ -2,9 +2,7 @@ source "https://supermarket.getchef.com"
 
 cookbook 'rvm', '~> 0.9.2'
 cookbook 'java', '~> 1.28.0'
-cookbook 'apache2', '~> 2.0.0'
-cookbook 'mysql', '~> 5.5.2'
 cookbook 'apt-repo', '~> 0.1.1'
-#cookbook 'apt', '~> 2.6.0'
-#cookbook 'td-agent', git: 'https://github.com/treasure-data/chef-td-agent.git'
 cookbook 'jenkins', '~> 2.1.2'
+cookbook 'atom', path: 'site-cookbooks/atom'
+cookbook 'td-agent-private', path: 'site-cookbooks/td-agent-private'
--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -2,15 +2,22 @@ require 'serverspec'
 include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
 include Serverspec::Helper::Debian
-include Serverspec::Helper::RedHat
 
 Rspec.configure do |c|
   c.before :all do
     c.os = backend(Serverspec::Commands::Base).check_os
   end
-  c.path = "/sbin:/user/sbin"
+  #c.path = "/sbin:/user/sbin"
 end
 
-describe package('apache2') do
+describe command('/usr/bin/java -version') do
+  its(:stdout) { should match /1.7/ }
+end
+
+describe file('/usr/bin/atom') do
+  it { should be_file }
+end
+
+describe package('td-agent') do
   it { should be_installed }
 end
