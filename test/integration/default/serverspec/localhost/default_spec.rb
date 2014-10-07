ab43f0d6cbeb841cf6d1b5f824c03a7bd8be3f9a
kanbara <soel754@gmail.com>
2014-10-07 18:55:33 +0900

    serverspec fix

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -1,14 +1,18 @@
 require 'serverspec'
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-include Serverspec::Helper::Debian
+#include Serverspec::Helper::Exec
+#include SpecInfra::Helper::Ssh
+#include Serverspec::Helper::DetectOS
+#include SpecInfra::Helper::DetectOS
+#include Serverspec::Helper::Debian
 
-Rspec.configure do |c|
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
+#Rspec.configure do |c|
+#  c.before :all do
+#    c.os = backend(Serverspec::Commands::Base).check_os
+#  end
   #c.path = "/sbin:/user/sbin"
-end
+#end
+
+set :backend, :exec
 
 describe command('/usr/bin/java -version') do
   its(:stdout) { should match /1.7/ }
